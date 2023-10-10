9ddbc67518529c93316cdf9c6949f3461372682e
mitsiayeva <147310373+mitsiayeva@users.noreply.github.com>
2023-10-10 13:31:44 +0200

    Add files via upload

---
+++ zajecia2/Zadanie1_AM.py
@@ -0,0 +1,46 @@
+# Exercise 1
+print("Exercise 1")
+print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut labore \net dolore magna aliqua.")
+print("\n")
+
+# Exercise 2
+print("Exercise 2")
+intX = 1 #integer
+floatX = 1.5 #float
+boolX = True #boolean
+boolY = False #boolean
+strX = "napis" #string
+strY = """napisnapisnapisnapis"""
+
+print ("Type of variable: ",type (floatX))
+print("\n")
+
+# Exercise 3
+print("Exercise 3")
+intX = 10
+intY = 4
+print ("X is: ", intX, " Y is: ", intY)
+print ("X + Y: ",intX+intY)
+print ("X - Y: ",intX-intY)
+print ("X * Y: ",intX*intY)
+print ("X / Y: ",intX/intY)
+print ("X ** Y: ",intX**intY)
+print ("X % Y: ",intX%intY)
+print("\n")
+
+#Exercise 4
+print("Exercise 4")
+print ("Equal: ",intX == intY)
+print ("Different: ",intX != intY)
+print ("X Bigger: ",intX>intY)
+print ("X Smaller: ",intX<intY)
+print ("X Bigger or equal: ",intX>=intY)
+print ("X Smaller or equal: ",intX<=intY)
+print("\n")
+
+#Exercise 5
+print ("Exercise 5")
+strX = "London"
+strY = "Paris"
+print(strX, "|", strY)
+
